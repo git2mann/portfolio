5b49fc4e3bfef0bfb1440f04402da347970c39a2
Leon Kimemia Nduati <130558246+git2mann@users.noreply.github.com>
2025-03-03 12:50:08 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 // Define metadata for the application (used for SEO and social sharing)
 export const metadata: Metadata = {
-  title: "Leon Nduati: A Portfolio",
+  title: SITE_NAME,
   description: "Personal portfolio and blog showcasing music, art, and projects",
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
