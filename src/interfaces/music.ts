dd9b8a143a20a07ce72c7bfb59594425b64cec82
Leon Kimemia Nduati <130558246+git2mann@users.noreply.github.com>
2025-03-17 00:22:09 +0300

    Create music.ts

---
+++ src/interfaces/music.ts
@@ -0,0 +1,22 @@
+
+export type Annotation = {
+  id: string;
+  text: string;
+  explanation: string;
+};
+
+export type Song = {
+  id: string;
+  title: string;
+  lyrics: Annotation[];
+  audioUrl?: string;
+  year: string;
+};
+
+export type Album = {
+  id: string;
+  title: string;
+  coverImage: string;
+  releaseYear: string;
+  songs: Song[];
+};
